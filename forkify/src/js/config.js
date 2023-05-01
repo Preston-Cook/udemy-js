51d6f2d1a22aff1837498a4ae0aa14b77d19d11e
Preston Cook <preston.l.cook@gmail.com>
2023-04-30 19:49:06 -0500

    finish course

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'c1de6693-a7d0-46b8-a49b-3fc5e4cb4f9c';
+export const MODAL_CLOSE_SEC = 2.5;
